77a3c0fa15754b3e414ca0fc53274120bef156fd
Una84 <ST10467455@rcconnect.edu.za>
2026-04-13 23:07:13 +0200

    input validation for user name and empty inputs
    Code for input Handler

---
+++ InputHandler.cs
@@ -0,0 +1,13 @@
+﻿public static class InputHandler
+{
+    public static string ValidateName(string name)
+    {
+        while (string.IsNullOrWhiteSpace(name))
+        {
+            Console.Write("⚠️ Name cannot be empty. Enter your name: ");
+            name = Console.ReadLine();
+        }
+
+        return name;
+    }
+}
