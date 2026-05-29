9723f0849ce8454659808230ad6502233f525919
Una84 <ST10467455@rcconnect.edu.za>
2026-05-29 18:42:03 +0200

    Create MemoryManager.cs

---
+++ MemoryManager.cs
@@ -0,0 +1,9 @@
+namespace CybersecurityAwarenessBot
+{
+    public class MemoryManager
+    {
+        public string UserName { get; set; }
+        public string FavouriteTopic { get; set; }
+        public string LastTopic { get; set; }
+    }
+}
