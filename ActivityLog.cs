d354db1afeca1e5a002d897248bf4dbf08e6ee9a
Una84 <ST10467455@rcconnect.edu.za>
2026-06-26 20:22:27 +0200

    Create ActivityLog.cs
    Part 3

---
+++ ActivityLog.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CybersecurityAwarenessBot
+{
+    public class ActivityLog
+    {
+        public DateTime TimeStamp { get; set; }
+
+        public string Action { get; set; }
+    }
+}
