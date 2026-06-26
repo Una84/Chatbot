5eca3842eaec17841e6064fcf4dc20f5aef003d8
Una84 <ST10467455@rcconnect.edu.za>
2026-06-26 20:24:13 +0200

    Update MemoryManager.cs
    Part 3

--- MemoryManager.cs
+++ MemoryManager.cs
@@ -3,7 +3,11 @@ namespace CybersecurityAwarenessBot
     public class MemoryManager
     {
         public string UserName { get; set; }
+
         public string FavouriteTopic { get; set; }
+
         public string LastTopic { get; set; }
+
+        public string LastSentiment { get; set; }
     }
 }
